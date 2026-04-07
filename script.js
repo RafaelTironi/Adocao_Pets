0e9ef6dc69c3e2542046b05a9cb8eeb5c4e43549
RafaelTironi-Casa <rafael_tironi1@estudante.sesisenai.org.br>
2026-04-07 20:10:07 -0300

    add: terminei o formulário e adicionei as variaveis ao javascript

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-document.getElementById("adoption-form").addEventListener("submit", function(e) {
+document.getElementById("adoption-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     let nome = document.getElementById("Nome").value;
     let email = document.getElementById("Email").value;
     let moradia = document.getElementById("Moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked').value;
+    let pets = document.querySelector('input[name="pets"]:checked').value;
+    let horas = document.getElementById("horas").value;
+    let motivo = document.getElementById("motivo").value;
+    let permite = document.querySelector('input[name="permite"]:checked').value;
+    let financeiro = document.querySelector('input[name="financeiro"]:checked').value;
+    let impulso = document.querySelector('input[name="impulso"]:checked').value;
+    let termos = document.querySelector('input[name="termos"]:checked').value;
 
-    if(nome.length < 3)  return alert("Nome Inválido");
+    if (nome.length < 3) return alert("Nome Inválido");
 
-        document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>"+"Nome:"  + nome;
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome:" + nome;
 });
